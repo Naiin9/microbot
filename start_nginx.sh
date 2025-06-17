7aa000725633044be88ca056d20853953e83b298
naiin9 <naiin9@outlook.com>
2025-06-17 18:39:04 +0700

    show more kube info

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-hostname=`hostname -f`
-sed -i "s/XXX/${hostname}/" /usr/share/nginx/html/index.html
+sed -i "s/NODE_NAME/${MY_NODE_NAME}/" /usr/share/nginx/html/index.html
+sed -i "s/NODE_IP/${MY_NODE_IP}/" /usr/share/nginx/html/index.html
+sed -i "s/POD_NAME/${MY_POD_NAME}/" /usr/share/nginx/html/index.html
+sed -i "s/POD_IP/${MY_POD_IP}/" /usr/share/nginx/html/index.html
 /usr/sbin/nginx -g "daemon off;"
